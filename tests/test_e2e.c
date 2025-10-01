eb8f94406a933f123f1678279969ab23bad7d418
Naplus-crypto <patiohat@gmail.com>
2025-10-01 13:57:39 +0700

    Update test_e2e.c

--- tests/test_e2e.c
+++ tests/test_e2e.c
@@ -1,1 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void run_e2e_test(void){
+    printf("Running E2E test...\n");
+    system("./crm < tests/e2e_input.txt > tests/e2e_output.txt");
+    printf("E2E test finished. Check tests/e2e_output.txt\n");
+}
