bb9afc35c1baa0ddcd09574a7a2c5d6d30a21dbd
Naplus-crypto <patiohat@gmail.com>
2025-10-03 10:23:11 +0700

    Update test_e2e.c

--- tests/test_e2e.c
+++ tests/test_e2e.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "customer_manager.c"
+
 int main() {
     int rc = system("./crm < tests/e2e_input.txt > tests/e2e_output.txt");
     if (rc != 0) { puts("[E2E] FAIL: crm execution error"); return 1; }
